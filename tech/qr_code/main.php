8c179ef020c7d88d66a16dcfde4d2275b9dc44cf
ElijahWong <wongwenwei@live.com>
2024-02-18 20:38:59 +0800

    made a rapidAPI link

--- tech/qr_code/main.php
+++ tech/qr_code/main.php
@@ -6,13 +6,14 @@
      </head>
 <div class="form max-w-md mx-auto mt-10">
   <h1 class="text-xl font-bold text-center mb-4">PHYMATH QR Code Generator</h1>
+  <p>pure static QR code, no tracking nor analytics</p>
   <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
     <div class="mb-4">
       <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="url" id="website" name="website" placeholder="https://phymath.com" required />
     </div>
     <div class="mb-4">
       Choose the color for QR code:
-      <input class="w-1/16" type="color" id="qrColor" name="qrColor" value="#ffd700" />
+      <input class="w-1/16" type="color" id="qrColor" name="qrColor" value="#000000" />
     </div>
     <div class="flex items-center justify-between">
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onclick="generateQRCode()">
