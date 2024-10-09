3f26607d2d2080185d246a51bbc63e291fb5a983
Sciwalker <wongwenwei@live.com>
2024-10-09 17:50:30 +0800

    corrected the link url of image

--- Mathematics Articles/2019/Hopf Fibration - stereographic projection.php
+++ Mathematics Articles/2019/Hopf Fibration - stereographic projection.php
@@ -57,7 +57,7 @@ Our world consists of 3 spatial dimensions and 1 time dimension. But how to perc
 
 <div class="row">
                 <div class="col-md-6">
-                    <img src="https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C2140%2C2000%7C91Hy1Ue0qOL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UX679_.png" alt="Hopf Fibration T-Shirt" class="img-responsive">
+                    <img src="https://drive.google.com/thumbnail?id=1EYYjrL6J4ebir0ivlSXA6h7WyJgUAOJb" alt="Hopf Fibration T-Shirt" class="img-responsive">
                 </div>
                 <div class="col-md-6">
                     <h4>Hopf Fibration T-Shirt</h4>
