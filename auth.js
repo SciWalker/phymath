3348ba811c801dda29883f3cd1fc9c7d87e28499
Sciwalker <wongwenwei@live.com>
2025-03-26 23:19:43 +0800

    added the incognito

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 const cognitoAuthConfig = {
     authority: "https://cognito-idp.ap-southeast-1.amazonaws.com/ap-southeast-1_g2114GZQr",
     client_id: "12ap4v63fqjdm1ld22nj79vhpe",
-    redirect_uri: "https://www.phymath.com/home.php",
+    redirect_uri: "https://www.phymath.com",
     response_type: "code",
     scope: "email openid profile"
 };
@@ -16,7 +16,7 @@ if (typeof oidc !== 'undefined') {
 
 function signOutRedirect() {
     const clientId = "12ap4v63fqjdm1ld22nj79vhpe";
-    const logoutUri = "https://www.phymath.com/home.php";
+    const logoutUri = "https://www.phymath.com";
     const cognitoDomain = "https://phymath.auth.ap-southeast-1.amazoncognito.com";
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
 }
